845f1b5472ee3a3e4dca5e893af6a5bc951aefdc
Carlos Zamora <czamora5@ucol.mx>
2022-10-09 05:21:27 -0500

    feat: implement skeleton loaders

--- src/app/app.js
+++ src/app/app.js
@@ -2,12 +2,28 @@ import API from './services/api.service.js';
 import formatNumber from './utils/formatNumber.js'
 
 export default async function run() {
+  // Setup
   const gallery = document.querySelector('.gallery');
   const cardTemplate = document.getElementById('card-template').content;
-  const frag = document.createDocumentFragment();
+  const cardSkeleton = document.getElementById('skeleton-template').content;
+  let frag = document.createDocumentFragment();
+
+  // Adding skeletons while we fetch the data from the API
+  for (let i = 0; i < 16; i++) {
+    const skeleton = cardSkeleton.cloneNode(true);
+    frag.appendChild(skeleton);
+  }
+  gallery.appendChild(frag);
 
   const characters = await API.getCharacters();
 
+  // We reassign our fragment and delete all the skeletons from the gallery
+  frag = document.createDocumentFragment();
+  while (gallery.lastElementChild) {
+    gallery.removeChild(gallery.lastElementChild);
+  }
+
+  // Adding cards to the html
   characters.forEach(character => {
     const card = cardTemplate.cloneNode(true);
 
